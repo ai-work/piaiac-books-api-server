f6e9d6e428e370a6ae62211feaf02c546496475e
Asif Iqbal <asifgvs@gmail.com>
2023-04-15 22:28:33 +0200

    books by id route

--- src/app/books/[id]/route.ts
+++ src/app/books/[id]/route.ts
@@ -6,10 +6,10 @@ export async function GET(request: Request, { params }: {
   }) {
     
     const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-    const { rows } = await pool.query(`SELECT * FROM books WHERE id = ${params.id}`);
-    console.log(rows);
+    const data = await pool.query(`SELECT * FROM books WHERE id = ${params.id}`);
+    console.log(data);
     // event.waitUntil(pool.end());  // doesn't hold up the response
-    return new Response(JSON.stringify(rows));
+    return new Response(JSON.stringify(data));
 }
 
 export const runtime = 'edge';
