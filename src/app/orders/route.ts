ecec422d1c01ca5dac7b4bd889a8b50a78e84889
Asif Iqbal <asifgvs@gmail.com>
2023-04-17 08:02:43 +0200

    orders POST route

--- src/app/orders/route.ts
+++ src/app/orders/route.ts
@@ -1,28 +1,36 @@
-import { NextResponse } from 'next/server';
+import { Pool } from '@neondatabase/serverless';
 
-export async function POST() {
+export async function POST(request: Request) {
     // {
     //     "bookId": 4,
     //     "quantity": 2,
     //     "customerName": "Asif"
     // }
 
+    let  { bookId, quantity, customerName }: {bookId: number, quantity: number, customerName: string} = await request.json();
+    if (!quantity || quantity == 0) {
+        quantity = 1;
+    }
 
-    const data = { id: "abcd1234", created: true };
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
-    // {
-    //     "created": true,
-    //     "orderId": "jOqtH-qcrFXaMdqv0sFr2"
-    // }
+    const { rows } = await pool.query('INSERT INTO orders(id, bookId, customerName, created, createdBy, quantity, timestamp) VALUES($1, $2, $3, $4, $5, $6) RETURNING id, created', 
+                                    [randomHash(21), bookId, customerName, true, quantity, (new Date).getTime()])
 
-    // respond!
-    const responseJson = JSON.stringify(data);
-    return new Response(responseJson, { headers: { 
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-    }});
-
-    // return NextResponse.json(data);
+    // event.waitUntil(pool.end());  // doesn't hold up the response
+    return new Response(JSON.stringify({rorderId: rows[0].id, created: rows[0].created}));
 }
+
+function randomHash(length: number) {
+    let pool = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_";
+    
+    let buf = "";
+  
+    for (let i = 0; i < length; i++) {
+      buf += pool.charAt(Math.floor(Math.random() * pool.length));
+    }
+  
+    return buf;
+  }
 
 export const runtime = 'edge';
