a06e11ab0a9fd12e85d893cb21b9c382cb37197e
Asif Iqbal <asifgvs@gmail.com>
2023-04-17 10:37:10 +0200

    orders POST route - createdBy hard-coded

--- src/app/orders/route.ts
+++ src/app/orders/route.ts
@@ -1,12 +1,7 @@
 import { Pool } from '@neondatabase/serverless';
 
 export async function POST(request: Request) {
-    // {
-    //     "bookId": 4,
-    //     "quantity": 2,
-    //     "customerName": "Asif"
-    // }
-
+    
     let  { bookId, quantity, customerName }: {bookId: number, quantity: number, customerName: string} = await request.json();
     if (!quantity || quantity == 0) {
         quantity = 1;
@@ -15,7 +10,7 @@ export async function POST(request: Request) {
     const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
     const { rows } = await pool.query('INSERT INTO orders(id, bookId, customerName, created, createdBy, quantity, timestamp) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id, created', 
-                                    [randomHash(21), bookId, customerName, true, 'self',quantity, ((new Date).getTime()/1000) as number])
+                                    [randomHash(21), bookId, customerName, true, 'self',quantity, Math.floor((new Date).getTime()/1000)])
 
     // event.waitUntil(pool.end());  // doesn't hold up the response
     return new Response(JSON.stringify({rorderId: rows[0].id, created: rows[0].created}));
