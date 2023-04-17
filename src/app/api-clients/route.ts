08cb1dffcc1c37f29e6125f7ea065f009e0eac3e
Asif Iqbal <asifgvs@gmail.com>
2023-04-17 07:45:15 +0200

    api_clients POST route - dummy token

--- src/app/api-clients/route.ts
+++ src/app/api-clients/route.ts
@@ -1,26 +1,27 @@
-import { NextResponse } from 'next/server';
+import { Pool } from '@neondatabase/serverless';
 
-export async function POST() {
-    // {
-    //     "clientName": "Postman",
-    //     "clientEmail": "valentin@example.com"
-    // }
+export async function POST(request: Request) {
+    const {clientName, clientEmail }: {clientName: string, clientEmail: string} = await request.json();
 
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
-    const data = { id: "abcd1234", created: true };
+    const { rows } = await pool.query('SELECT * FROM api_clients WHERE clientEmail = $1', [clientEmail]);
+    let apiClientId: number = 0;
+    if (!rows || rows.length === 0) {
+        const data1 = await pool.query('INSERT INTO api_clients(clientName, clientEmail) VALUES($1, $2) RETURNING id', 
+                                        [clientName, clientEmail]);
+        apiClientId = data1.rows[0].id;
+    } else {
+        apiClientId = rows[0].id
+    }
+    
+    const clientAccessToken = '5791hger';
+    const data2 = await pool.query('INSERT INTO api_client_tokens(apiClientId, accessToken) VALUES($1, $2) RETURNING accessToken', 
+                                    [apiClientId, clientAccessToken])
 
-    // {
-    //     "accessToken": "jOqtH-qcrFXaMdqv0sFr2"
-    // }
+    // event.waitUntil(pool.end());  // doesn't hold up the response
+    return new Response(JSON.stringify({accessToken: clientAccessToken}));
 
-    // respond!
-    const responseJson = JSON.stringify(data);
-    return new Response(responseJson, { headers: { 
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-    }});
-
-    // return NextResponse.json(data);
 }
 
 export const runtime = 'edge';
